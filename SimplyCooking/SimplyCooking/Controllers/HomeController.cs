32ded98b9dbd0263ebc860b2774e3ab1111965d9
CzarnyMudzyn <hbsmandzich@gmail.com>
2018-03-14 10:56:02 +0100

    Lista z szczegółami :)

--- SimplyCooking/SimplyCooking/Controllers/HomeController.cs
+++ SimplyCooking/SimplyCooking/Controllers/HomeController.cs
@@ -13,6 +13,7 @@ using System.Web;
 using System.Web.Mvc;
 using SimplyCooking.Models;
 
+
 namespace SimplyCooking.Controllers
 {
     public class HomeController : Controller
@@ -38,5 +39,11 @@ namespace SimplyCooking.Controllers
 
             return View();
         }
+        [HttpPost]
+public ActionResult Akcja(string imie)
+        {
+            return View();
+        }
+
     }
 }
